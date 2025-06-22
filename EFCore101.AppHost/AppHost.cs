0368cd84172c28ed386eac92e7ba50b41acb4ae4
Shazni Shiraz <shazni.shiraz@ascentic.se>
2025-06-22 17:33:47 +0530

    .NET Aspire PostgreSQL integration Hosting

--- EFCore101.AppHost/AppHost.cs
+++ EFCore101.AppHost/AppHost.cs
@@ -2,7 +2,18 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
+var username = builder.AddParameter("username", "admin");
+var password = builder.AddParameter("password", "admin");
+
+var postgres = builder.AddPostgres("postgres", username, password)
+    .WithDataBindMount(
+        source: @"C:\PostgreSQL\EFCore101",
+        isReadOnly: false);
+
+var postgresdb = postgres.AddDatabase("postgresdb");
+
 var api = builder.AddProject<EFCore101_API>("api")
+    .WithReference(postgresdb)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
