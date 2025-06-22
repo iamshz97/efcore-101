0eb3912547d387a75fee434d8ac3f1909de8608e
Shazni Shiraz <shazni.shiraz@ascentic.se>
2025-06-22 21:57:25 +0530

    Creating our first database migration

---
+++ EFCore101.API/Migrations/20250622162649_InitialCreate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EFCore101.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, comment: "The unique identifier of the book"),
+                    Title = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false, comment: "The title of the book"),
+                    Description = table.Column<string>(type: "character varying(1000)", maxLength: 1000, nullable: false, comment: "The description of the book"),
+                    ImageUrl = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, comment: "The date and time the book was created"),
+                    UpdatedAt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false, comment: "The date and time the book was updated")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
