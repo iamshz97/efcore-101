454e62fd6c82d4f895944094075b5e31600ff8ec
Shazni Shiraz <shazni.shiraz@ascentic.se>
2025-06-22 21:54:15 +0530

    Approach #1 - Use of data annotations to specify props configs

--- EFCore101.API/Entities/Book.cs
+++ EFCore101.API/Entities/Book.cs
@@ -1,6 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 public class Book : BaseEntity<Guid>
 {   
     public required string Title { get; set; }
+
+    [Comment("The description of the book")]
+    [Required]
+    [MaxLength(1000)]
     public required string Description { get; set; }
     public required string ImageUrl { get; set; }
 }
