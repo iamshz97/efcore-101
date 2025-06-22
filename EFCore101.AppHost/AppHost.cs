88b81cc852b0585934fbcc348b659826031111db
Shazni Shiraz <shazni.shiraz@ascentic.se>
2025-06-22 16:53:37 +0530

    Reference api project in dotnet aspire

--- EFCore101.AppHost/AppHost.cs
+++ EFCore101.AppHost/AppHost.cs
@@ -1,3 +1,8 @@
+using Projects;
+
 var builder = DistributedApplication.CreateBuilder(args);
+
+var api = builder.AddProject<EFCore101_API>("api")
+    .WithExternalHttpEndpoints();
 
 builder.Build().Run();
