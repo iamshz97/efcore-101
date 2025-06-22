66734e26fcad943ae27fa15a2b8e2ddcf9f51eb4
Shazni Shiraz <shazni.shiraz@ascentic.se>
2025-06-22 20:37:03 +0530

    Add books entity into dbcontext

--- EFCore101.API/Context/EFCore101DbContext.cs
+++ EFCore101.API/Context/EFCore101DbContext.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 
 public class EFCore101DbContext : DbContext
 {
+    public DbSet<Book> Books { get; set; } = null!;
+
     public EFCore101DbContext(DbContextOptions<EFCore101DbContext> options)
         : base(options)
     {
